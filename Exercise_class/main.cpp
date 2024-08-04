33ae2dff21dc92c639b5bece19252a6de052c57a
Alexander Nesterov <alex18nest@icloud.com>
2024-08-04 09:40:31 -0300

    Initial Commit

---
+++ Exercise_class/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Exercise_class
+//
+//  Created by Александр Нестеров on 8/4/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
